cf77ee177cb5a2277b531f409e1e7b857b9f84ea
marcusx2 <mogsilva@yahoo.com.br>
2015-08-18 15:57:52 -0700

    versao sequencial funcionando 100%

--- main.c
+++ main.c
@@ -8,13 +8,13 @@ Eg: main teste.txt*/
 
 
 /*divide matriz A e matriz B(aka vetor B) pela diagonal da Matriz A, e faz diagonal de Matriz A ser 0. Isso faz a matriz A virar matriz A*(matriz A modificada, aka MAstar) e a matriz B virar matriz B*(matriz B modificada, aka MBstar). Ver pdf explicando o metodo de Jacobi. Recebe como parametro a ordem da matriz e as matrizes*/
-void MAandMBdividebydiagonal(int J_ORDER, double** MA, double MB[]);
+void MAandMBdividebydiagonal(int J_ORDER, double** MA, double MB[], double** MAstar, double MBstar[]);
 /*multiplica a matriz A* por Xk. Ver pdf explicando o metodo de Jacobi.Recebe como parametro a ordem da matriz e as matrizes e retorna a matriz(vetor) resultante*/
-double* MAstartimesXk(int J_ORDER, double** MA, double Xk[]);
+double* MAstartimesXk(int J_ORDER, double** MAstar, double Xk[]);
 /*subtrai matriz B* por matrizA* multiplicado por Xk. Faz parte da formula da iteratividade do metodo de Jacobi. Retorna a matriz(vetor) resultante. Recebe como parametro a ordem da matriz, a matriz MB* e a matriz A* multiplicado por Xk(MAstartimes)*/
-double* sub_MBMAstartimesXk(int J_ORDER, double MB[], double MAstartimes[]);
-/*aplica o metodo de Jacobi propriamente dito. Recebe a ordem da matriz, numero maximo de iteracoes permitido, erro, matriz MA* e matriz MB* */
-void JacobiRichardson(int J_ORDER, int J_ITE_MAX, double* J_ERROR, double** MA, double MB[]);
+double* sub_MBMAstartimesXk(int J_ORDER, double MBstar[], double MAstartimes[]);
+/*aplica o metodo de Jacobi propriamente dito. Recebe a ordem da matriz, numero maximo de iteracoes permitido, linha pra testar, erro, matriz MA* e matriz MB* */
+void JacobiRichardson(int J_ORDER, int J_ITE_MAX, int J_ROW_TEST, double* J_ERROR, double** MA, double MB[], double** MAstar, double MBstar[]);
 /*calcula erro pela formula do pdf. Recebe ordem da matriz, o vetor de X atual(X) e o vetor de X anterior(Xk)*/
 double get_error(int J_ORDER, double X[], double Xk[]);
 /*funcao auxiliar para retornar o maior valor de um vetor, recebe ordem do vetor e o vetor. Retorna o maior valor encontrado. Eh usado para calcular o erro*/
@@ -26,74 +26,87 @@ void printMatrix(int J_ORDER, double** Matrix);
 /*pega as informacoes do arquivo de acordo com a especificacao do trabalho e salva nos parametros passado como argumento da funcao.
 Os parametros tem exatamente o mesmo nome da descricao do trabalho, todos devem ser passados por referencia, inclusive as matrizes, porque elas sao mallocadas dentro da funcao*/
 void readStuffFromFile(FILE* pFile, int* J_ORDER, int* J_ROW_TEST, double* J_ERROR, int* J_ITE_MAX, double*** MA, double** MB);
-/*para dar free na matriz MA* e MB* */
-void cleanup(FILE* pFile, int J_ORDER, double** MA, double MB[]);
+/*para dar free nas matrizes MA, MB,  MA* e MB* e fechar arquivo de entrada*/
+void cleanup(FILE* pFile, int J_ORDER, double** MA, double* MB, double** MAstar, double MBstar[]);
+/*para calcular resultado em determinada linha. Recebe vetor X final, matriz MA original e a linha que deve ser usada*/
+double result_row(double* X, double** MA, int J_ROW_TEST, int J_ORDER);
 
 int main(int argc, char* argv[]) { 
 	FILE* pFile;//ponteiro pro arquivo de entrada
     pFile = fopen (argv[1] , "r");//abre arquivo de entrada para leitura somente
     int J_ORDER, J_ROW_TEST, J_ITE_MAX;//variaveis que vao armazenar valores do arquivo. Ordem da matriz, linha de teste, maximo de iteracao
     double J_ERROR;//armazena erro
-    double** MA = NULL;//matriz A, que vai se tornar matriz A*(matriz A modificada) posteriormente, na funcao MAandMBdividebydiagonal
-    double* MB = NULL;//matriz(vetor) B, que vai se tornar matriz B*(matriz B modificada) posteriormente, na funcao MAandMBdividebydiagonal
+    double** MA = NULL;//matriz A original
+    double* MB = NULL;//matriz(vetor) B original
     readStuffFromFile(pFile, &J_ORDER, &J_ROW_TEST, &J_ERROR, &J_ITE_MAX, &MA, &MB);//le o arquivo e armazena nas variaveis os valores
-    MAandMBdividebydiagonal(J_ORDER, MA, MB);//divide MA e MB pelos valores da diagonal de MA, produzindo MA* e MB*
-    JacobiRichardson(J_ORDER, J_ITE_MAX, &J_ERROR, MA, MB);//aplica metodo de Jacobi
-    cleanup(pFile, J_ORDER, MA, MB);//da free nos malloc de MA e MB, e fecha arquivo de entrada
+	double** MAstar = (double**) malloc(sizeof(double*) * J_ORDER);//matriz A*(matriz A modificada), gerada na funcao MAandMBdividebydiagonal
+	for (int i = 0; i < J_ORDER; ++i) MAstar[i] = (double*) malloc(sizeof(double) * J_ORDER);
+	double* MBstar = (double*) malloc(sizeof(double) * J_ORDER);//matriz B*(matriz B modificada), gerada na funcao MAandMBdividebydiagonal
+    MAandMBdividebydiagonal(J_ORDER, MA, MB, MAstar, MBstar);//divide MA e MB pelos valores da diagonal de MA, produzindo MA* e MB*
+    JacobiRichardson(J_ORDER, J_ITE_MAX, J_ROW_TEST, &J_ERROR, MA, MB, MAstar, MBstar);//aplica metodo de Jacobi
+    cleanup(pFile, J_ORDER, MA, MB, MAstar, MBstar);//da free nos malloc de MA e MB, e fecha arquivo de entrada
 }
 
-void MAandMBdividebydiagonal(int J_ORDER, double** MA, double MB[]) {
-	
+void MAandMBdividebydiagonal(int J_ORDER, double** MA, double MB[], double** MAstar, double MBstar[]) {
     for (int i = 0; i < J_ORDER; ++i) {
         for (int j = 0; j < J_ORDER; ++j) {
-            if (i != j) {
-                MA[i][j] /= MA[i][i];//divide todo mundo da linha pela diagonal
-            }
+				MAstar[i][j] = MA[i][j]/MA[i][i];//divide todo mundo da linha pela diagonal
         }
-        MB[i] /= MA[i][i];//divide a matriz B pela diagonal
-        MA[i][i] = 0;//faz a diagonal ser 0
+		MBstar[i] = MB[i]/MA[i][i];//divide a matriz B pela diagonal
+        MAstar[i][i] = 0;//faz a diagonal ser 0
     }
 }
 
-void JacobiRichardson(int J_ORDER, int J_ITE_MAX, double* J_ERROR, double** MA, double MB[]) {
-    int iterations = 0;
+void JacobiRichardson(int J_ORDER, int J_ITE_MAX, int J_ROW_TEST, double* J_ERROR, double** MA, double MB[], double** MAstar, double MBstar[]) {    
+	int iterations = 1;
     double e = 100;
     double* X;
     double* MAstartimes;
     double* Xk = (double*) malloc(sizeof(double) * J_ORDER);//aloca Xk para armazenar sempre a iteracao anterior.
-    memcpy(Xk, MB, sizeof(double) * J_ORDER);//inicialmente Xk(na primeira iteracao, o X0) recebe os valores do vetor B*
+    memcpy(Xk, MBstar, sizeof(double) * J_ORDER);//inicialmente Xk(na primeira iteracao, o X0) recebe os valores do vetor B*
 
     while (e > *J_ERROR && iterations < J_ITE_MAX) {//enquanto for maior que erro permitido e nao atingir numero maximo de iteracao
         ++iterations;//incremente numero de iteracoes
-        MAstartimes = MAstartimesXk(J_ORDER, MA, Xk);//calcula MA* vezes Xk
-        X = sub_MBMAstartimesXk(J_ORDER, MB, MAstartimes);//subtrai MB* do resultado anterior. X eh o Xk+1
+        MAstartimes = MAstartimesXk(J_ORDER, MAstar, Xk);//calcula MA* vezes Xk
+        X = sub_MBMAstartimesXk(J_ORDER, MBstar, MAstartimes);//subtrai MB* do resultado anterior. X eh o Xk+1
         e = get_error(J_ORDER, X, Xk);//calcula erro
         memcpy(Xk, X, sizeof(double) * J_ORDER);//atualiza Xk
 
     }
-	printf("iterations: %d\n", iterations);//imprime iteracoes
-	printArray(J_ORDER, X);//imprime vetor final
+
+	//imprime numero de iteracoes e resultado em determinada linha
+	printf("iterations: %d\nRowTest: %d => [%f] =? %f\n", iterations, J_ROW_TEST, result_row(X, MA, J_ROW_TEST, J_ORDER), MB[J_ROW_TEST]);
+	//printArray(J_ORDER, X);//imprime vetor final
     free(X);
     free(Xk);
     free(MAstartimes);
 }
 
-double* MAstartimesXk(int J_ORDER, double** MA, double Xk[]) {
+double result_row(double* X, double** MA, int J_ROW_TEST, int J_ORDER) {
+	double result = 0.0;
+	for (int i = 0; i < J_ORDER; ++i) {
+		result += MA[J_ROW_TEST][i] * X[i];
+	}
+
+	return result;
+}
+
+double* MAstartimesXk(int J_ORDER, double** MAstar, double Xk[]) {
 	//multiplicacao de matriz por vetor
     double* vect = (double*) calloc(sizeof(double), J_ORDER);
     for (int i = 0; i < J_ORDER; ++i) {
         for (int j = 0;  j < J_ORDER; ++j) {
-            vect[i] += MA[i][j] * Xk[j];
+            vect[i] += MAstar[i][j] * Xk[j];
         }
     }
     return vect;
 }
 
-double* sub_MBMAstartimesXk(int J_ORDER, double MB[], double MAstartimes[]) {
+double* sub_MBMAstartimesXk(int J_ORDER, double MBstar[], double MAstartimes[]) {
 	//subtrai MB* de MA* vezes Xk
     double* vect = (double*) malloc(sizeof(double) * J_ORDER);
     for (int i = 0; i < J_ORDER; ++i) {
-        vect[i] = MB[i] - MAstartimes[i];
+        vect[i] = MBstar[i] - MAstartimes[i];
     }
     return vect;
 
@@ -141,12 +154,15 @@ void printMatrix(int J_ORDER, double** Matrix) {
     }
 }
 
-void cleanup(FILE* pFile, int J_ORDER, double** MA, double* MB) {
+void cleanup(FILE* pFile, int J_ORDER, double** MA, double* MB, double** MAstar, double MBstar[]) {
     for (int i = 0; i < J_ORDER; ++i){
         free(MA[i]);
+		free(MAstar[i]);
     }
     free(MA);
+	free(MAstar);
     free(MB);
+	free(MBstar);
     fclose(pFile);
 }
 
